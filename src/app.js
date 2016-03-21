d206679f1ccb02c887d280af948662f125407208
Tushar Pokle <tushar.pokle@gmail.com>
2016-03-21 21:01:05 +1100

    Stage1

--- src/app.js
+++ src/app.js
@@ -1,9 +1,3 @@
 // This is the main entry point of the application
 
-import React from 'react'
-import ReactDOM  from 'react-dom'
-
-ReactDOM.render(
-	<h1>Edit me in src/app.js</h1>,
-  document.getElementById('app')
-);
+import _ from './stage1'
